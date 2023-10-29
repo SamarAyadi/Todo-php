c74aaa355817fc0a3aa35c59ba02e9d6fc2b972f
Samar AYADI <ayadisamardev@gmail.com>
2023-10-29 23:56:21 +0100

    Mise en place du CSS

--- index.php
+++ index.php
@@ -5,14 +5,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="public/css/style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
     <script defer src="public/js/index.js"></script>
     <title>Todo</title>
 </head>
 
 <body>
-    <header>HEADER</header>
-    <div class="conent">CONTENT</div>
-    <footer>FOOTER</footer>
+    <div class="container">
+        <header>
+            <div class="logo">PinguCoder Todo</div>
+        </header>
+        <div class="content">
+            <div class="todo-container">
+                <h1>Ma Todo</h1>
+                <div class="todo-form"></div>
+                <div class="todo-list"></div>
+            </div>
+        </div>
+        <footer>2023 © Tous droits réservés</footer>
+    </div>
 
 
 </body>
