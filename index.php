c4a9b091d00b1290f5a859258bd949de73e17017
Samar AYADI <ayadisamardev@gmail.com>
2023-10-29 23:11:56 +0100

    Mise en place

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/css/style.css">
+    <script defer src="public/js/index.js"></script>
+    <title>Todo</title>
+</head>
+
+<body>
+    <header>HEADER</header>
+    <div class="conent">CONTENT</div>
+    <footer>FOOTER</footer>
+
+
+</body>
+
+</html>
