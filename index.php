b6da8abb7dc0f5032a6ad88a65d96f63ddfa37ed
Samar AYADI <ayadisamardev@gmail.com>
2023-10-30 00:43:39 +0100

    Implémentation de la logique du formulaire

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
+<?php
+const ERROR_REQUIRED = '🚩  Veuillez renseigner une todo ';
+const ERROR_TOO_SHORT = '🚩  Veuillez entrer au moins 5 caractères ';
+$error = '';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $todo = $_POST['todo'] ?? '';
+
+    if (!$todo) {
+        $error = ERROR_REQUIRED;
+    } else if (mb_strlen($todo) < 5) {
+        $error = ERROR_TOO_SHORT;
+    }
+}
+?>
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -12,10 +32,13 @@
         <div class="content">
             <div class="todo-container">
                 <h1>Ma Todo</h1>
-                <form class="todo-form" action="/" method="post">
-                    <input type="text">
+                <form class="todo-form" action="/" method="POST">
+                    <input name="todo" type="text">
                     <button class="btn btn-primary">Ajouter</button>
                 </form>
+                <?php if ($error) : ?>
+                <p class="text-danger"><?= $error ?></p>
+                <?php endif; ?>
                 <div class="todo-list"></div>
             </div>
         </div>
