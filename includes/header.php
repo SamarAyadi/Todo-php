a1b9e9c0e91b968ebfec2d7ebeeed3223fdbc33d
Samar AYADI <ayadisamardev@gmail.com>
2023-10-30 00:06:32 +0100

    Utilisation de plusieurs fichiers PHP

---
+++ includes/header.php
@@ -0,0 +1,3 @@
+<header>
+    <div class="logo">PinguCoder Todo</div>
+</header>
--- index.php
+++ index.php
@@ -2,20 +2,13 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="public/css/style.css">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
-    <script defer src="public/js/index.js"></script>
+    <?php require_once 'includes/head.php' ?>
     <title>Todo</title>
 </head>
 
 <body>
     <div class="container">
-        <header>
-            <div class="logo">PinguCoder Todo</div>
-        </header>
+        <?php require_once 'includes/header.php' ?>
         <div class="content">
             <div class="todo-container">
                 <h1>Ma Todo</h1>
@@ -23,7 +16,8 @@
                 <div class="todo-list"></div>
             </div>
         </div>
-        <footer>2023 © Tous droits réservés</footer>
+        <?php require_once 'includes/footer.php' ?>
+
     </div>
 
 
